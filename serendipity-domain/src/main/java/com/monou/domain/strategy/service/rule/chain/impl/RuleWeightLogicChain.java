c2a7196b3a623ec240488a2075693539e9958e64
gao <igao107@163.com>
2024-06-17 23:09:49 +0800

    fixed: 修正概率计算；RuleWeightLogicChain权重配置检验完善；RuleLockLogicFilter防止空指针异常

--- serendipity-domain/src/main/java/com/monou/domain/strategy/service/rule/chain/impl/RuleWeightLogicChain.java
+++ serendipity-domain/src/main/java/com/monou/domain/strategy/service/rule/chain/impl/RuleWeightLogicChain.java
@@ -43,9 +43,10 @@ public class RuleWeightLogicChain extends AbstractLogicChain {
 
         String ruleValue = repository.queryStrategyRuleValue(strategyId, ruleModel());
 
-        // 1. 根据用户ID查询用户抽奖消耗的积分值，本章节我们先写死为固定的值。后续需要从数据库中查询。
+        // 1. 解析权重规则值 4000:102,103,104,105 拆解为；4000 -> 4000:102,103,104,105 便于比对判断
         Map<Long, String> analyticalValueGroup = getAnalyticalValue(ruleValue);
         if (null == analyticalValueGroup || analyticalValueGroup.isEmpty()) {
+            log.warn("抽奖责任链-权重告警【策略配置权重，但ruleValue未配置相应值】 userId: {} strategyId: {} ruleModel: {}", userId, strategyId, ruleModel());
             return null;
         }
 
